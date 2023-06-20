b8afd6a12f162d0031a0dbcab7e8920dd6c4a731
pychangeric <pychangeric@gmail.com>
2023-06-20 12:01:32 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,13 @@ class BirdsController < ApplicationController
     end
   end
 
+  # POST /birds
+  def create
+    byebug
+  end
+  def create
+    bird = Bird.create(name: ???, species: ???)
+    render json: bird, status: :created
+  end
+
 end
